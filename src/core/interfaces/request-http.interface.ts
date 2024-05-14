4c16264c4e1e6dff1c45d1f3448c05d3a8695408
BetoNajera <ing.betonajera@gmail.com>
2024-05-14 15:17:31 -0600

    :sparkles: Feat: Create http interfaces
    Create ExceptionHttpInterface and ResponseHttpInterface

---
+++ src/core/interfaces/request-http.interface.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage } from "http";
+
+export interface RequestHttpInterface extends IncomingMessage {
+  /**
+   * Request parameters
+   *
+   * @example {id: '8jnv9n4c9iicn4i...'}
+   */
+  params: Record<string, string>,
+
+  /**
+   * Query parameters
+   *
+   * @example {limit: 5, offset: 0}
+   */
+  query: Record<string, string | string[] | undefined>,
+}
