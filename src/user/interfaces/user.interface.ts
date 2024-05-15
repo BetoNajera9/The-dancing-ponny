2dd2a94e6bcbc9575592d7ba31a6ed8a064f856b
BetoNajera <ing.betonajera@gmail.com>
2024-05-15 11:22:05 -0600

    :sparkles: Feat: Create interface
    Create UserInterface

---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,17 @@
+import { Document } from "mongoose"
+
+export interface UserInterface extends Document {
+  /**
+   * Nick name of the user
+   *
+   * @example Sméagol
+   */
+  nickName: string
+
+  /**
+   * Password to the user
+   *
+   * @example ijcnirnv09j4inciun
+   */
+  password: string
+}
