728714eabb94340c869ab4a5012d80d2f7f64e11
BetoNajera <ing.betonajera@gmail.com>
2024-05-16 18:45:22 -0600

    :sparkles: Feat: Implement pagination and search queries to routes
    Implement pagination and search queries to routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,24 @@
 import { ServerResponse } from 'http'
 
 import { handlerException, handlerResponse } from '../common/utils'
+import { PaginationInterface } from '../common/interfaces'
 import { RequestHttpInterface } from '../core/interfaces'
+import { QueryValidator } from '../common/decorators'
+import { PaginationSchema } from '../common/schemas'
 import { UserService } from './user.service'
 import { UserInterface } from './interfaces'
 
 export class UserController {
-	private userService = new UserService()
-
+	@QueryValidator([PaginationSchema])
 	async getUsers(req: RequestHttpInterface, res: ServerResponse) {
 		try {
-			const users = await this.userService.getAllUsers()
+			const pagination: PaginationInterface = {
+				page: +req.query.page,
+				take: +req.query.take,
+			}
+
+			const userService = new UserService()
+			const users = await userService.getAllUsers(pagination)
 
 			handlerResponse<UserInterface[]>(res, users, 'Users found successful')
 		} catch (error) {
