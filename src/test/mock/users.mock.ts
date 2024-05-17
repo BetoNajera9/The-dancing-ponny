da4b74bd9beccc8798c5d7937af1f8a327f9cbb1
BetoNajera <ing.betonajera@gmail.com>
2024-05-17 13:35:11 -0600

    :sparkles: Feat: Create test mocks
    Create test mocks

---
+++ src/test/mock/users.mock.ts
@@ -0,0 +1,11 @@
+export const UserMock = {
+	_id: 'user123',
+	nickName: 'userBlock',
+	password: '$2b$10$82EvpIo3BLdpzKwv5Wr6De9SunYOxGatB0VkBnwrsnc.mcBXhirrO',
+}
+
+export const UserBlockMock = {
+	_id: 'user123',
+	nickName: 'user',
+	password: '$2b$10$82EvpIo3BLdpzKwv5Wr6De9SunYOxGatB0VkBnwrsnc.mcBXhirrO',
+}
